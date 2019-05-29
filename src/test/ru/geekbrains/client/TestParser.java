1f1cc431c509675af5b246f47600c45494c3a96a
Dias <Dias64@mail.ru>
2019-05-29 22:49:40 +0300

    ExecutorService

--- src/test/ru/geekbrains/client/TestParser.java
+++ src/test/ru/geekbrains/client/TestParser.java
@@ -2,26 +2,18 @@ package ru.geekbrains.client;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static ru.geekbrains.client.MessagePatterns.parseTextMessage;
-import static ru.geekbrains.client.MessagePatterns.parseTextMessageRegx;
+import static org.junit.Assert.assertTrue;
+import static ru.geekbrains.client.MessagePatterns.*;
 
 /**
  * Unit тесты для различных методов сетевого чата
  * Прежде чем запускать, необходимо отметить папку test как Test Source Root
  */
 public class TestParser {
-
-    @Test
-    public void testParser() {
-        TextMessage textMessage = parseTextMessage("/w userFrom Example of message", "userTo");
-
-        assertNotNull(textMessage);
-        assertEquals("userFrom", textMessage.getUserFrom());
-        assertEquals("Example of message", textMessage.getText());
-        assertEquals("userTo", textMessage.getUserTo());
-    }
 
     @Test
     public void testParserRegx() {
@@ -34,7 +26,38 @@ public class TestParser {
     }
 
     @Test
-    public void testParseConnectedMessage() {
-        // TODO написать тест для метода MessagePattern.ParseConnectedMessage
+    public void testParser() {
+        TextMessage textMessage = parseTextMessage("/w userFrom Example of message", "userTo");
+
+        assertNotNull(textMessage);
+        assertEquals("userFrom", textMessage.getUserFrom());
+        assertEquals("Example of message", textMessage.getText());
+        assertEquals("userTo", textMessage.getUserTo());
     }
+
+    @Test
+    public void testParseUserList() {
+        Set<String> userList = parseUserList("/user_list ivan petr");
+
+        assertNotNull(userList);
+        assertTrue(userList.contains("ivan"));
+        assertTrue(userList.contains("petr"));
+    }
+
+    @Test
+    public void testParseConnectedMessage() {
+        String user = parseConnectedMessage("/connected ivan");
+
+        assertNotNull(user);
+        assertEquals("ivan", user);
+    }
+
+    @Test
+    public void testParseDisconnectedMessage() {
+        String user = parseDisconnectedMessage("/disconnect petr");
+
+        assertNotNull(user);
+        assertEquals("petr", user);
+    }
+
 }
